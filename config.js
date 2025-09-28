5b420b171ee245a124bf757efc2631a140daa31c
AsmiPL <167706065+AsmiPL@users.noreply.github.com>
2025-09-28 10:45:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
      {
       name: "GlaceYT",
       password: "glaceyt",
-      host: "5.39.63.207",
+      host: "5.39.63.207:8262",
       port:  8262,
       secure: false
     }
