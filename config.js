23accf9c5deae2c8000e094d680909ffce0ad6c3
AsmiPL <167706065+AsmiPL@users.noreply.github.com>
2026-01-17 14:53:59 +0100

    Update configuration settings in config.js
    Updated MongoDB URI, changed activity name to Spotify, adjusted embed timeout, and modified node configuration.

--- config.js
+++ config.js
@@ -4,23 +4,23 @@ module.exports = {
   TOKEN: "MTIzODQ5ODAxNTEyNDEyNzc4NA.Gt2Tj4.c8NlNOzjT1Z5H-yGY_fzOc6XXtBw4RolcKRnY8",
   language: "pl",
   ownerID: ["882279843398570026", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "4bfb37679f754afa9a93676d3807eb65",
   spotifyClientSecret : "7a1f0521991449d789ea196b6c90ad69",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Spotify", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
-  embedTimeout: 86220, 
+  embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "",
-      password: "Kaun.Yuvraj",
-      host: "pnode1.danbot.host",
-      port:  1186,
+      name: "GlaceYT",
+      password: "glace",
+      host: "de-01.strixnodes.com",
+      port:  2010,
       secure: false
     }
   ]
